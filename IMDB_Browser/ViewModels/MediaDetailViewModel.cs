5c2cf5dd6f47a178751566c1c81c042ec37725d7
heosmond <osmondhe@gmail.com>
2025-04-09 19:50:01 -0300

    Get genres in details view

--- IMDB_Browser/ViewModels/MediaDetailViewModel.cs
+++ IMDB_Browser/ViewModels/MediaDetailViewModel.cs
@@ -1,12 +1,19 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using IMDB_Browser.Data;
 using IMDB_Browser.Models;
 using IMDB_Browser.Navigation;
+using Microsoft.EntityFrameworkCore;
 
 namespace IMDB_Browser.ViewModels
 {
     public class MediaDetailViewModel : INotifyPropertyChanged, IParameterReceiver
     {
+        private readonly ImdbContext _dbContext;
         private Title _selectedTitle;
+        private ObservableCollection<Genre> _genres;
 
         public Title SelectedTitle
         {
@@ -15,7 +22,24 @@ namespace IMDB_Browser.ViewModels
             {
                 _selectedTitle = value;
                 OnPropertyChanged(nameof(SelectedTitle));
+                _ = LoadGenresAsync(); // Load genres when the selected title changes
             }
+        }
+
+        public ObservableCollection<Genre> Genres
+        {
+            get => _genres;
+            set
+            {
+                _genres = value;
+                OnPropertyChanged(nameof(Genres));
+            }
+        }
+
+        public MediaDetailViewModel(ImdbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _genres = new ObservableCollection<Genre>();
         }
 
         public void ReceiveParameter(object parameter)
@@ -23,6 +47,18 @@ namespace IMDB_Browser.ViewModels
             if (parameter is Title title)
             {
                 SelectedTitle = title;
+            }
+        }
+
+        private async Task LoadGenresAsync()
+        {
+            if (SelectedTitle != null)
+            {
+                var genres = await _dbContext.Genres
+                    .Where(g => g.Titles.Any(t => t.TitleId == SelectedTitle.TitleId))
+                    .ToListAsync();
+
+                Genres = new ObservableCollection<Genre>(genres);
             }
         }
 
