cb14a19a9ba97b46eb02fafc4704225e5fd8c9ae
Nasfurion <diego.amadio93@gmail.com>
2025-04-10 13:49:11 -0300

    Some tests done

---
+++ TestProject1/FavouritesViewModel.Test.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IMDB_Browser.ViewModels;
+using System.Collections.ObjectModel;
+using System.Linq;
+using IMDB_Browser.Models;
+
+namespace TestProject1
+{
+    [TestClass]
+    public sealed class FavouritesViewModelTests
+    {
+        private FavouritesViewModel _viewModel;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _viewModel = new FavouritesViewModel();
+        }
+
+        [TestMethod]
+        public void UpdateFavorites_AddsAndRemovesFavoritesCorrectly()
+        {
+            var title = new Title { PrimaryTitle = "Test Movie", IsFavorite = true };
+
+            _viewModel.UpdateFavorites(title);
+            Assert.IsTrue(_viewModel.FavMovies.Contains(title));
+
+            title.IsFavorite = false;
+            _viewModel.UpdateFavorites(title);
+            Assert.IsFalse(_viewModel.FavMovies.Contains(title));
+        }
+
+        [TestMethod]
+        public void SearchQuery_FiltersMoviesCorrectly()
+        {
+            var title1 = new Title { PrimaryTitle = "Test Movie 1", IsFavorite = true };
+            var title2 = new Title { PrimaryTitle = "Another Movie", IsFavorite = true };
+
+            _viewModel.UpdateFavorites(title1);
+            _viewModel.UpdateFavorites(title2);
+
+            _viewModel.SearchQuery = "Test";
+            Assert.AreEqual(1, _viewModel.FavMovies.Count);
+            Assert.AreEqual(title1, _viewModel.FavMovies.First());
+
+            _viewModel.SearchQuery = "Another";
+            Assert.AreEqual(1, _viewModel.FavMovies.Count);
+            Assert.AreEqual(title2, _viewModel.FavMovies.First());
+
+            _viewModel.SearchQuery = string.Empty;
+            Assert.AreEqual(2, _viewModel.FavMovies.Count);
+        }
+
+        [TestMethod]
+        public void ToggleFavoriteCommand_TogglesFavoriteStatus()
+        {
+            var title = new Title { PrimaryTitle = "Test Movie", IsFavorite = false };
+
+            _viewModel.ToggleFavoriteCommand.Execute(title);
+            Assert.IsTrue(title.IsFavorite);
+
+            _viewModel.ToggleFavoriteCommand.Execute(title);
+            Assert.IsFalse(title.IsFavorite);
+        }
+
+        [TestMethod]
+        public void ToggleWatchlistCommand_TogglesWatchlistStatus()
+        {
+            var title = new Title { PrimaryTitle = "Test Movie", IsInWatchlist = false };
+
+            _viewModel.ToggleWatchlistCommand.Execute(title);
+            Assert.IsTrue(title.IsInWatchlist);
+
+            _viewModel.ToggleWatchlistCommand.Execute(title);
+            Assert.IsFalse(title.IsInWatchlist);
+        }
+    }
+}
---
+++ TestProject1/HomeViewModel.Test.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using IMDB_Browser.Models;
+using IMDB_Browser.Navigation;
+using IMDB_Browser.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class HomeViewModelTests
+    {
+        private Mock<INavigationService> _navigationServiceMock;
+        private Mock<FavouritesViewModel> _favouritesViewModelMock;
+        private Mock<WatchListViewModel> _watchListViewModelMock;
+        private Mock<MainViewModel> _mainViewModelMock;
+        private HomeViewModel _homeViewModel;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _navigationServiceMock = new Mock<INavigationService>();
+            _favouritesViewModelMock = new Mock<FavouritesViewModel>();
+            _watchListViewModelMock = new Mock<WatchListViewModel>();
+            _mainViewModelMock = new Mock<MainViewModel>(_navigationServiceMock.Object, _favouritesViewModelMock.Object, _watchListViewModelMock.Object);
+
+            _homeViewModel = new HomeViewModel(_navigationServiceMock.Object, _favouritesViewModelMock.Object, _watchListViewModelMock.Object, _mainViewModelMock.Object);
+        }
+
+        [TestMethod]
+        public void Titles_Setter_ShouldTriggerFilterTitles()
+        {
+            // Arrange
+            var titles = new ObservableCollection<Title>
+            {
+                new Title { PrimaryTitle = "Movie 1", TitleType = "movie", StartYear = 2020 },
+                new Title { PrimaryTitle = "Movie 2", TitleType = "movie", StartYear = 2021 }
+            };
+
+            // Act
+            _homeViewModel.Titles = titles;
+
+            // Assert
+            Assert.AreEqual(2, _homeViewModel.FilteredTitles.Count);
+        }
+
+        [TestMethod]
+        public void SearchQuery_Setter_ShouldTriggerFilterTitles()
+        {
+            // Arrange
+            var titles = new ObservableCollection<Title>
+            {
+                new Title { PrimaryTitle = "Movie 1", TitleType = "movie", StartYear = 2020 },
+                new Title { PrimaryTitle = "Movie 2", TitleType = "movie", StartYear = 2021 }
+            };
+            _homeViewModel.Titles = titles;
+
+            // Act
+            _homeViewModel.SearchQuery = "Movie 1";
+
+            // Assert
+            Assert.AreEqual(1, _homeViewModel.FilteredTitles.Count);
+            Assert.AreEqual("Movie 1", _homeViewModel.FilteredTitles.First().PrimaryTitle);
+        }
+
+    }
+}
